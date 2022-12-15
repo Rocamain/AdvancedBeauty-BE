b3138abd65b223c6b83b9e70879d55b5e7e77a86
Rocamain <fjrocavazquez@gmail.com>
2022-12-15 00:29:52 +0000

    add(test data) && modify(gitignore and package,json scripts):

---
+++ data/test-data/bookings.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    "shop_id": 1,
+    "service_id": 4,
+    "customer_id": 15,
+    "appointment": "2023-06-24T13:00:00.000Z",
+    "appointment_finish": "2023-06-24T13:30:00.000Z"
+  },
+  {
+    "shop_id": 1,
+    "service_id": 9,
+    "customer_id": 11,
+    "appointment": "2023-08-08T16:00:00.000Z",
+    "appointment_finish": "2023-08-08T17:00:00.000Z"
+  },
+  {
+    "shop_id": 2,
+    "service_id": 9,
+    "customer_id": 15,
+    "appointment": "2022-10-15T09:00:00.000Z",
+    "appointment_finish": "2022-10-15T10:00:00.000Z"
+  },
+  {
+    "shop_id": 2,
+    "service_id": 2,
+    "customer_id": 7,
+    "appointment": "2023-03-11T12:00:00.000Z",
+    "appointment_finish": "2023-03-11T12:30:00.000Z"
+  },
+  {
+    "shop_id": 3,
+    "service_id": 4,
+    "customer_id": 7,
+    "appointment": "2023-03-17T20:00:00.000Z",
+    "appointment_finish": "2023-03-17T20:30:00.000Z"
+  },
+  {
+    "shop_id": 3,
+    "service_id": 6,
+    "customer_id": 11,
+    "appointment": "2023-04-24T13:00:00.000Z",
+    "appointment_finish": "2023-04-24T14:00:00.000Z"
+  }
+]
